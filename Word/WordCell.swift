75f9baf084c4159913ffecaf77563c1e89e9c73a
Christopher Lee <clee95@u.rochester.edu>
2021-10-23 15:13:48 -0400

    added a custom cell and designed it

---
+++ Word/WordCell.swift
@@ -0,0 +1,26 @@
+//
+//  WordCell.swift
+//  Word
+//
+//  Created by Lee, Christopher on 10/23/21.
+//
+
+import UIKit
+
+class WordCell: UITableViewCell {
+
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var titleCell: UILabel!
+    @IBOutlet weak var subtitleCell: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
